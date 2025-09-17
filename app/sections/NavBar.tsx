b490530b0c3f09aa6e212fd0de9545ab50dcb444
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-09-17 23:17:28 +0100

    cleaning navbar

--- app/sections/NavBar.tsx
+++ app/sections/NavBar.tsx
@@ -138,15 +138,6 @@ export default function NavBar({
                   </motion.button>
                 ))}
               </motion.div>
-
-              <motion.div
-                initial={{ y: 20, opacity: 0 }}
-                animate={{ y: 0, opacity: 1 }}
-                transition={{ delay: 0.4, duration: 0.3 }}
-                className="px-4 pb-8"
-              >
-                <Button />
-              </motion.div>
             </div>
           </motion.div>
         )}
